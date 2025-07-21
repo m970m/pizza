7e5cff7231af20f7195c29aad6edd82bf07a449f
Vladimir Byzov <woosaiw@gmail.com>
2025-07-21 15:40:44 +0300

    Добавил healthcheck эндпоинт.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
+});
+
+Route::get('/health', function() {
+    try
+    {
+        DB::connection()->getPdo();
+
+        return response()->json([
+            'status' => 'ok',
+        ], 200);
+
+    } catch (Exception $e)
+    {
+        return response()->json([
+            'status' => 'error',
+            'error' => 'Database connection failed'
+        ], 503);
+    }
 });
